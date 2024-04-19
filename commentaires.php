b2cf6344e5dd5363c23cbb0cad0bb7095ca59c60
Teromun <160504641+Teromun@users.noreply.github.com>
2024-04-19 10:14:50 +0200

    Add files via upload

---
+++ commentaires.php
@@ -0,0 +1,9 @@
+<h1>Message bien reçu !</h1>
+        
+<div class="card">
+    
+    <div class="card-body">
+        <h5 class="card-title">Rappel des informations</h5>
+        <p class="card-text"><b>Votre commenraire</b> : <?php echo $_GET['conseil']; ?></p>
+    </div>
+</div>
